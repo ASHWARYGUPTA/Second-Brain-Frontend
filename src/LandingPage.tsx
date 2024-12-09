72a24add90746647f4a1249b5a9881a7f7bfeace
ASHWARYGUPTA <ashwarygupta13@gamil.com>
2024-12-10 02:48:43 +0530

    Solving GAuth Problem

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,13 +1,17 @@
 import { useEffect } from "react"
 import { NavBar } from "./components/ui/NavBar"
+import { useNavigate } from "react-router-dom"
 
 export const LandingPage= ()=>{
+    const navigate = useNavigate();
     useEffect(()=>{
         document.title = "Landing Page"
     },[])
     return <div>
         <div>
-            <NavBar textButton1="SignUp" textButton2="SignIn"/>
+            <NavBar textButton1="SignUp" textButton2="SignIn" onClickButton1={()=>{navigate("/signup")}}
+                onClickButton2={()=>{navigate("/signin")}}
+                />
         </div>
             
     </div>
