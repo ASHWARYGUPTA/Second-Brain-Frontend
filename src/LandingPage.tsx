d99729d8ad2b0ec53daa7ff874bc06f4321779dd
ASHWARYGUPTA <ashwarygupta13@gamil.com>
2024-12-23 00:09:03 +0530

    Adding Dynamic Landing Page

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -2,28 +2,60 @@ import { useEffect } from "react"
 import { NavBar } from "./components/ui/NavBar"
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom";
+import { useRecoilValue, useRecoilValueLoadable } from "recoil";
+import { isLoggedIn } from "./components/ui/States/RecoilAtoms";
+import { Loader } from "./components/ui/Loader";
 
 const LandingPage= ()=>{
     const navigate = useNavigate();
+    const isLoggedInVal = useRecoilValueLoadable(isLoggedIn);
     useEffect(()=>{
         document.title = "Landing Page"
     },[])
     return <div>
+        {isLoggedInVal.state === "loading"?
+        <Loader/>
+        :
+        <>{isLoggedInVal.state === "hasValue" && isLoggedInVal.contents === true?
         <div>
-            <NavBar textButton1="SignUp" textButton2="SignIn" onClickButton1={()=>{navigate("/signup")}}
-                onClickButton2={()=>{navigate("/signin")}}
-                />
-        </div>
-        <div className="flex justify-center items-center h-screen w-screen">
             <div>
+                {isLoggedInVal.contents === true?
+                <NavBar 
+                    textButton1="SignUp" 
+                    textButton2="SignIn" 
+                    onClickButton1={()=>{navigate("/signup")}}
+                    onClickButton2={()=>{navigate("/signin")}}
+                />:<NavBar
+                    textButton1="DashBoard" 
+                    textButton2="SignUp" 
+                    onClickButton1={()=>{navigate("/dashboard")}}
+                    onClickButton2={()=>{navigate("/signup")}}
+                    />}
+            </div>
+            <div className="flex justify-center items-center h-screen w-screen">
+                {isLoggedInVal.contents === false?
                 <div>
-                    <Link to = "/signin"> <span className="text-purple-600">Sign In</span></Link>&nbsp;to continue
-                </div>
+                    <div>
+                        <Link to = "/signin"> <span className="text-purple-600">Sign In</span></Link>&nbsp;to continue
+                    </div>
+                    <div>
+                        Or Try <Link to = "/signin"><span className="text-purple-600">Signing Up</span></Link>&nbsp;to continue
+                    </div>
+                </div>:
+                <>
                 <div>
-                    Or Try <Link to = "/signin"><span className="text-purple-600">Signing Up</span></Link>&nbsp;to continue
+                    <Link to ="/dashboard"><span className="text-purple-600">Go to DashBoard</span></Link>
                 </div>
+                </>
+                }
+                
             </div>
         </div>
+        :<>
+            ERROR OCCURED
+        </>
+        }</>
+        }
     </div>
 }
 
