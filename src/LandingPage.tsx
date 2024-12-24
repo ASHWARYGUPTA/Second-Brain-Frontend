fc2ed34b0823b135a942b2f5c4bc0fe89a6da0c4
ASHWARYGUPTA <ashwarygupta13@gamil.com>
2024-12-24 17:36:59 +0530

    Updating Landing Page

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -13,49 +13,53 @@ const LandingPage= ()=>{
         document.title = "Landing Page"
     },[])
     return <div>
-        {isLoggedInVal.state === "loading"?
-        <Loader/>
-        :
-        <>{isLoggedInVal.state === "hasValue" && isLoggedInVal.contents === true?
-        <div>
-            <div>
-                {isLoggedInVal.contents === false?
+        {isLoggedInVal.state === "loading" ? <Loader/>:<>
+            {isLoggedInVal.state === "hasValue" && isLoggedInVal.contents === true?
+            <>
+                <NavBar
+                    textButton1="DashBoard" 
+                    textButton2="SignUp" 
+                    onClickButton1={()=>{navigate("/dashboard")}}
+                    onClickButton2={()=>{navigate("/signup")}}
+                    />
+                    <div className="flex justify-center items-center h-screen w-screen">
+                        <div>
+                            <Link to ="/dashboard"><span className="text-purple-600">Go to DashBoard</span></Link>
+                        </div>
+                    </div>
+
+                
+            </>:<>
+            {isLoggedInVal.contents === false ? <>
                 <NavBar 
                     textButton1="SignUp" 
                     textButton2="SignIn" 
                     onClickButton1={()=>{navigate("/signup")}}
                     onClickButton2={()=>{navigate("/signin")}}
-                />:<NavBar
-                    textButton1="DashBoard" 
-                    textButton2="SignUp" 
-                    onClickButton1={()=>{navigate("/dashboard")}}
-                    onClickButton2={()=>{navigate("/signup")}}
-                    />}
-            </div>
-            <div className="flex justify-center items-center h-screen w-screen">
-                {isLoggedInVal.contents === true?
-                <div>
-                    <div>
-                        <Link to = "/signin"> <span className="text-purple-600">Sign In</span></Link>&nbsp;to continue
-                    </div>
-                    <div>
-                        Or Try <Link to = "/signin"><span className="text-purple-600">Signing Up</span></Link>&nbsp;to continue
-                    </div>
-                </div>:
-                <>
-                <div>
-                    <Link to ="/dashboard"><span className="text-purple-600">Go to DashBoard</span></Link>
+                />
+                    <div className="flex justify-center items-center h-screen w-screen">
+                        <div>
+                                <Link to = "/signin"> <span className="text-purple-600">Sign In</span></Link>&nbsp;to continue
+                        </div>
+                        <div>
+                                Or Try <Link to = "/signin"><span className="text-purple-600">Signing Up</span></Link>&nbsp;to continue
+                        </div>
                 </div>
-                </>
-                }
-                
-            </div>
-        </div>
-        :<>
-            ERROR OCCURED
-        </>
-        }</>
-        }
+            </>
+            :
+            <>
+            <NavBar 
+                    textButton1="SignUp" 
+                    textButton2="SignIn" 
+                    onClickButton1={()=>{navigate("/signup")}}
+                    onClickButton2={()=>{navigate("/signin")}}
+            />
+            Error Occured
+            </>
+            }
+            </>
+            }
+        </>}
     </div>
 }
 
